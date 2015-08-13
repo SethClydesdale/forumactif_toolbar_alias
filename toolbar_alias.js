380cfc5735d6016cff92f0ab313f4f9dfe502091
Seth Clydesdale <sethc1995@gmail.com>
2015-08-13 08:56:35 -0400

    Update toolbar_alias.js

--- toolbar_alias.js
+++ toolbar_alias.js
@@ -1,12 +1,12 @@
 $(function() {
-  var toolbar_lang = {
-    
+  var toolbar_alias = {
+ 
     Share : 'Share',
     Login : 'Login',
     Register : 'Register',
     Welcome : 'Welcome',
     Notifications : 'Notifications',
-    
+ 
     See_my_profile : 'View profile',
     Edit_profile : 'Edit profile',
     All_Topics : 'My topics',
@@ -14,7 +14,7 @@ $(function() {
     js_topics_followed : 'Watched topics',
     Admin_panel : 'Administration Panel',
     Logout : 'Log out',
-    
+ 
     Notif_see_all : 'See all the notifications',
     Notif_priv_msg : 'You have received a <a href="/privmsg?folder=inbox&amp;nid=%(nid)s">private message</a> from <a href="/u%(id)d">%(name)s</a>',
     Notif_report : '<a href="/u%(id)d">%(name)s</a> has created a <a href="/report?nid=%(nid)s">message report</a>',
@@ -29,14 +29,14 @@ $(function() {
     Notif_topic_watch_p_guest : 'A guest wrote a message in <a href="/t%(topic_id)dp%(start)d-%(topic_name)s?nid=%(nid)s#%(post_id)d">a topic you watch</a>',
     Notif_mention : '<a href="/u%(id)d">%(name)s</a> tagged you in <a href="/t%(topic_id)dp%(start)d-%(topic_name)s?nid=%(nid)s#%(post_id)d">a topic</a>',
     Notif_hashtag : 'The keyword <a href="/tags/%(tag)s">#%(tag)s</a> has been tagged in <a href="/t%(topic_id)dp%(start)d-%(topic_name)s?nid=%(nid)s#%(post_id)d">a topic</a>.',
-    
+ 
     All_PMs : 'My private messages',
     No_assigned_rank : 'No special rank assigned',
     Posts : 'Posts',
     PMs : 'PMs',
     Reputation : 'Reputation'
-    
+ 
   },i;
-  
-  if (window._lang) for (i in toolbar_lang) _lang[i] = toolbar_lang[i];
+ 
+  if (window._lang) for (i in toolbar_alias) window._lang[i] = toolbar_alias[i];
 });
